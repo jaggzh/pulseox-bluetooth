bb09d0bdedfc9fb288566174b5eaac3950c84936
Jag <jag@h>
2022-01-15 08:00:03 -0800

    added settings to -example

--- settings-sample.py
+++ settings-sample.py
@@ -12,7 +12,11 @@ pomac='BA:03:C4:FF:FF:FF'
 do_web_lcd=True              # Hit LCD display with bpm and spo2
 do_web_lcd=False
 ip_lcd="192.168.3.10"        # Not used if do_web_display is False
+lcd_w=320
+lcd_h=240
 
+alert_audio=False            # Make a pysine noise when vitals out of range
+alert_audio=True
 
 # Data format/Model info should eventually be here, if others add
 # more models
