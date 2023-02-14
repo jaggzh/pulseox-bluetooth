95496246d423369729fb5205a43213f1072aed8b
Jag <jag@h>
2023-02-14 09:11:36 -0800

    modified callbacks example

--- usercallbacks-example.py
+++ usercallbacks-example.py
@@ -1,9 +1,11 @@
-# Copy this to usercallbacks.py
+# Copy this to usercallbacks.py, and edit if you need some events called
 
 def pre_display_normal():
 	print("This is called before the BPM/O2 drawing is done")
+	#os.system("lcd-notice -s 35 /mnt/shared/decorations/heart-metallic.png -y 20 -x 200 >/dev/null 2>/dev/null");
 
 def post_display_normal():
-	print("This is called after the BPM/O2 drawing is done")
+	#print("This is called after the BPM/O2 drawing is done")
+	pass
 
 # vim: set et
